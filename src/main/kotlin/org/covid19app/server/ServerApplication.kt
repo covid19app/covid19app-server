8a7dc217f53a23dec9bd1d3c7720b1c24486ebbf
Jan Prach <jendap@gmail.com>
2020-03-18 00:08:11 -0700

    add minimalistic healthcheck /health

--- src/main/kotlin/org/covid19app/server/ServerApplication.kt
+++ src/main/kotlin/org/covid19app/server/ServerApplication.kt
@@ -3,10 +3,22 @@ package org.covid19app.server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @EnableJpaRepositories
 @SpringBootApplication
 class ServerApplication
+
+@CrossOrigin
+@RestController
+class HealthController() {
+	@GetMapping("/health")
+	fun health(): String {
+		return "HEALTHY"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ServerApplication>(*args)
